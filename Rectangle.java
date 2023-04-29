e186dfff7e465b6460fef437fb0a62e0a3ada414
Gowthammggowda1999 <mggowthamgowda@gmail.com>
2023-04-29 18:41:02 +0530

    hii

---
+++ Rectangle.java
@@ -0,0 +1,17 @@
+class Rectangle
+{
+	
+	public static void main(String[] args) 
+	{
+		Rectangle r1=new Rectangle();
+		r1.area();
+	}
+	void area()
+	{
+		int w=1;
+		int h=7;
+	    int result=w*h;
+		System.out.println(result);
+    }
+
+}
