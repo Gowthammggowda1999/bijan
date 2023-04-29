c4db2b151d480cfceab66c91ac4f89eac66edcb3
Gowthammggowda1999 <mggowthamgowda@gmail.com>
2023-04-29 18:43:10 +0530

    hiia

---
+++ Square.java
@@ -0,0 +1,16 @@
+class Square
+{
+	public static void main(String[] args)
+	{
+		Square s1=new Square();
+		s1.area();
+	}
+	void area()
+	{
+		int r=7;
+	        int result=r*r;
+		System.out.println(result);
+         }
+
+}
+
